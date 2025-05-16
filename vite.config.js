d816ac7c9849b2c1cfcb24ad1c733ce7b750ee3e
Ian Destura <idestura35@gmail.com>
2025-05-16 09:42:17 +0800

    done: fix env

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
+import dotenv from 'dotenv';
+dotenv.config();
+
 export default defineConfig({
-  base: '/',
+  base: process.env.APP_ENV == 'GITHUB' ? process.env.GITHUB_BASE_URL : '/',
   plugins: [
     vue(),
     tailwindcss(),
