226cd5e6ebb44a50aceb8a32787a0c962f7added
Ian Destura <idestura35@gmail.com>
2025-05-16 09:43:30 +0800

    done: fix route

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../pages/home/index.vue'
 import Careers from '../pages/careers/index.vue'
 import CareersDetails from '../pages/careers-details/index.vue'
@@ -22,7 +22,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
